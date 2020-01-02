1e6302d4666d468c2220f991dc2d585ff5f4b3c1
Marcin <m.stolvrczyk@gmail.com>
2020-01-02 18:57:39 +0100

    creating client side

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,41 @@
+<template>
+<v-container fluid>
+  <v-row>
+    <v-col>
+      {{scrapeData.details}}
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<script>
+import ScrapeService from '../services/ScrapeService'
+export default {
+  name: 'Dashboard',
+  data () {
+    return {
+      scrapeService: new ScrapeService(),
+      scrapeData: null
+    }
+  },
+  methods: {
+    async scrapePage () {
+      let page = await this.scrapeService.getPage()
+      this.scrapeData = page
+      console.log(page)
+    }
+  },
+  mounted () {
+    this.scrapePage()
+  },
+  watch: {
+    'scrapeData' (value) {
+      console.log(value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
